3e98f2c1ac708b94bf5dc001d9b99aa979167149
Suraj Deshmukh <suraj@kinvolk.io>
2018-10-18 18:42:11 +0530

    installation steps for hugo

--- local-bin/update-gotools.sh
+++ local-bin/update-gotools.sh
@@ -4,4 +4,12 @@ set -euo pipefail
 
 go get -u -v mvdan.cc/sh/cmd/shfmt
 go get -u -v github.com/golang/dep/cmd/dep
-go get -u -v --tags extended github.com/gohugoio/hugo
+
+
+mkdir -p $HOME/git/hugo
+cd $HOME/git
+git clone https://github.com/gohugoio/hugo.git hugo
+cd hugo
+git pull --ff origin master
+go install --tags extended
+
