3f7f62bfcb4f0a6ec7cf88a9b3e7303e6d5c0624
Suraj Deshmukh <suraj@kinvolk.io>
2018-11-14 18:27:41 +0530

    update pullgo to install without specifying the latest version

--- local-bin/pullgo.sh
+++ local-bin/pullgo.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
 
-if [ -z $version ]; then
-    echo "Please provide the go version, by exporting variable 'version',
-    export version=\"go1.10.2\""
-    exit 1
-fi
+set -euo pipefail
 
 # check if git is installed, if not install it
-if ! [ -x "$(command -v git)" ]; then
-    echo "Installing git..."
-    sudo dnf -y install git
+if ! [[ -x "$(command -v git)" ]]; then
+  echo "Installing git..."
+  sudo dnf -y install git
 fi
 
+# referenced from https://github.com/udhos/update-golang/blob/master/update-golang.sh
+function get_latest_release() {
+  version=$(curl --silent https://storage.googleapis.com/golang |
+    grep -E -o 'go[0-9\.]+' |
+    grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' |
+    sort -V |
+    uniq |
+    tail -1)
+}
 
-# check if wget is installed
-if ! [ -x "$(command -v wget)" ]; then
-    echo "Installing wget..."
-    sudo dnf -y install wget
+# find what the latest version is
+get_latest_release
+echo "Latest golang ${version}"
+
+# if latest is already installed no need to download again
+if ls /usr/local/go > /dev/null 2>&1; then
+  if [[ $(go version) = *"${version}"* ]]; then
+    echo "Latest $(go version) already installed!"
+    exit 0
+  fi
 fi
 
-# download it in random location
+# download it in random temporary location
 rand=$RANDOM
 path=/tmp/goinstall-$rand
 mkdir -p $path
 cd $path
-
-file=$version.linux-amd64.tar.gz
+file=go$version.linux-amd64.tar.gz
 url=https://dl.google.com/go/$file
 
 echo "Downloading from $url"
-wget $url
-if [ $? -ne 0 ]; then
-    echo "Go downloading failed!"
-    exit 1
+if ! curl --silent -LO "${url}"; then
+  echo "Go downloading failed!"
+  exit 1
 fi
 
-echo "Uninstalling $(go version)"
-sudo rm -rf /usr/local/go
+# if already go is present uninstall it
+if ls /usr/local/go > /dev/null 2>&1; then
+  echo "Uninstalling $(go version)"
+  sudo rm -rf /usr/local/go
+fi
 
 echo "Installing new $version"
-sudo tar -C /usr/local -xzf $file
-if [ $? -ne 0 ]; then
-    echo "Go installing failed!"
-    exit 1
+if ! sudo tar -C /usr/local -xzf "${file}"; then
+  echo "Go installing failed!"
+  exit 1
 fi
 
 sudo ln -sf /usr/local/go/bin/go /usr/local/sbin/go
