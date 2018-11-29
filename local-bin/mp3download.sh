ea38198bcdede51c324f9556148958c5c729c1f7
Suraj Deshmukh <suraj@kinvolk.io>
2018-11-29 13:12:08 +0530

    update mp3download with shell format standard

--- local-bin/mp3download.sh
+++ local-bin/mp3download.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+#
+# This script helps you to convert the YouTube video into mp3 file.
 
-youtube-dl --extract-audio --audio-format mp3 $1
+set -euo pipefail
+
+function err() {
+  echo "$*" >&2
+}
+
+url="$*"
+if [[ -z "${url}" ]]; then
+  err "error: Please provide the YouTube video URL"
+  err ""
+  err "Usage:"
+  err ""
+  err "mp3download.sh https://youtube.com/randomvideo"
+  exit 1
+fi
+
+youtube-dl --extract-audio --audio-format mp3 "${url}"
