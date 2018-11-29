513f70dbe164406ccb0006277f2e4ab0faf91868
Suraj Deshmukh <suraj@kinvolk.io>
2018-11-29 12:40:44 +0530

    install everything with one script

---
+++ installers/install-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# As more installer scripts are added to the `installers` dir make an entry here
+# so install-all does really install everything.
+
+set -euo pipefail
+
+./installers/install-local-bin.sh
+./installers/install-configs.sh
