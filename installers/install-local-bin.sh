0c0a63549a8cc69a92364cc8e76790fa5104b124
Suraj Deshmukh <suraj@kinvolk.io>
2019-01-12 14:24:12 +0530

    add support to install private scripts

--- installers/install-local-bin.sh
+++ installers/install-local-bin.sh
@@ -2,15 +2,21 @@
 
 set -euo pipefail
 
-
-echo "Installing scripts from ./local-bin dir"
 mkdir -p ~/.local/bin
 
-# all the files in the 'local-bin' directory will be 
+echo "Installing scripts from ./local-bin dir ..."
+# all the files in the 'local-bin' directory will be
 # symlinked in ~/.local/bin
 for filename in local-bin/*; do
     sym=$(basename $filename)
     ln -sf `pwd`/$filename ~/.local/bin/$sym
 done
+echo "Installation successful for public scripts!"
 
-echo "Installation successful."
+echo "Installing scripts from ./dotfilesprivate/local-bin ..."
+# Now install the private scripts
+for filename in dotfilesprivate/local-bin/*; do
+    sym=$(basename $filename)
+    ln -sf `pwd`/$filename ~/.local/bin/$sym
+done
+echo "Installation successful for private scripts!"
