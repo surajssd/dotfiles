ee452c5532a1cb8f92e3aa40699cf0dd1ab68f34
Suraj Deshmukh <suraj@kinvolk.io>
2019-10-17 18:29:52 +0530

    touchpad: Add script to fix touchpad
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

---
+++ local-bin/fix-touchpad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This is used to fix the touchpad as mentioned here:
+# https://askubuntu.com/q/1091635/246019
+
+set -euo pipefail
+
+sudo sh -c 'echo -n "elantech"> /sys/bus/serio/devices/serio1/protocol'
