487fe8428a1fe3f93991165e9875b4e6b10cb3bd
Suraj Deshmukh <surajd.service@gmail.com>
2019-12-05 14:32:31 +0530

    fastmirror: Add from scripts repo
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

---
+++ local-bin/fastmirror.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+#===================================================================
+# add fastest mirror
+cat /etc/dnf/dnf.conf | grep 'fastestmirror'
+if [ $? -ne 0 ]; then
+    echo 'fastestmirror=true
+deltarpm=true
+' | sudo tee -a /etc/dnf/dnf.conf
+
+fi
+
