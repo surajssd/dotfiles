a7f887696796c3b875daa8c8f1ff7f943e5282eb
Suraj Deshmukh <surajd.service@gmail.com>
2020-03-04 12:25:37 +0530

    add terraform config

--- installers/install-configs.sh
+++ installers/install-configs.sh
@@ -7,5 +7,6 @@ echo "Installing configs from ./configs dir"
 ln -sf `pwd`/configs/bashrc ~/.bashrc
 ln -sf `pwd`/configs/gitconfig ~/.gitconfig
 ln -sf `pwd`/configs/gitignore ~/.gitignore
+ln -sf `pwd`/configs/terraformrc ~/.terraformrc
 
 echo "Installation successful."
