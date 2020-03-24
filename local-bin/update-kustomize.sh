464f6068a465904f7cc80a03e04e2eae0890d361
Suraj Deshmukh <surajd.service@gmail.com>
2020-03-24 12:12:32 +0530

    Add update-kustomize.sh

---
+++ local-bin/update-kustomize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | sh
+
+mv kustomize ~/.local/bin/
+echo "Downloaded successfully in ~/.local/bin/"
