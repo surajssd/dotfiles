2c17089da2729e911885a3d54eec69cbdc98356f
Suraj Deshmukh <surajd.service@gmail.com>
2020-10-07 13:34:28 +0530

    Update github CLI: Add new script
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

---
+++ local-bin/update-gh-cli.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# shellcheck source=/dev/null
+source "$(dirname "${BASH_SOURCE[0]}")"/util.sh
+
+# create this directory which is in path in linux distros
+# like fedora, centos and ubuntu.
+mkdir -p ~/.local/bin/
+
+# find what the latest version is
+get_latest_release cli/cli
+onlyversion=$(echo "${version}" | cut -d'v' -f2)
+
+# goto temp dir to download artifacts and then move it
+cd $(mktemp -d)
+
+echo "Downloading gh ${version}"
+curl --silent -LO https://github.com/cli/cli/releases/download/"${version}"/gh_"${onlyversion}"_linux_amd64.tar.gz && \
+  tar -xvzf gh_"${onlyversion}"_linux_amd64.tar.gz && \
+  mv gh_"${onlyversion}"_linux_amd64/bin/gh ~/.local/bin
+
+echo "Downloaded successfully in ~/.local/bin/"
