1d58a813806a2c01c7dd3ab59f27bca463dc67b3
Suraj Deshmukh <surajd.service@gmail.com>
2021-06-23 08:54:08 +0530

    Add new join-pdfs.sh
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

---
+++ local-bin/join-pdfs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+files=$@
+if [ -z "${files}" ]; then
+    echo "Please provide the list of PDF files."
+    echo "join-pdfs.sh 1.pdf 2.pdf"
+    exit 1
+fi
+
+set -euo pipefail
+
+tmpdir="$(mktemp -d)"
+cp -r "${files}" "${tmpdir}"
+
+cd "${tmpdir}"
+combined_name="combined-pdf-$(date.sh).pdf"
+pdfunite "${files}" "${combined_name}"
