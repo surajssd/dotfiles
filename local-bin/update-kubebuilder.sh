b5a6220a046f5aa12802395c5f13ddcdc52560cf
Suraj Deshmukh <surajd.service@gmail.com>
2021-09-15 19:28:25 +0530

    Fix update-kubebuilder.sh script
    This script is now minimal and easy to understand. It also replicates the
    commands as specified by the upstream:
    https://book.kubebuilder.io/quick-start.html#installation.
    
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- local-bin/update-kubebuilder.sh
+++ local-bin/update-kubebuilder.sh
@@ -2,21 +2,9 @@
 
 set -euo pipefail
 
-# shellcheck source=/dev/null
-source "$(dirname "${BASH_SOURCE[0]}")"/util.sh
+cd $(mktemp -d)
+# download kubebuilder and install locally.
+curl -sL -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
+chmod +x kubebuilder && mv kubebuilder ~/.local/bin/
 
-os=$(go env GOOS)
-arch=$(go env GOARCH)
-
-# find what the latest version is
-get_latest_release kubernetes-sigs/kubebuilder
-echo "Downloading kubebuilder ${version}"
-onlyversion=$(echo "${version}" | cut -d'v' -f2)
-
-# download kubebuilder and extract it to tmp
-curl -L "https://go.kubebuilder.io/dl/${onlyversion}/${os}/${arch}" | tar -xz -C /tmp/
-
-# move to a long-term location and put it on your path
-# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-mv "/tmp/kubebuilder_${onlyversion}_${os}_${arch}/bin/kubebuilder" ~/.local/bin/
 echo "Downloaded successfully in ~/.local/bin/"
