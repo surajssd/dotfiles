3abda2aa39408ea5f82302164306b1a6de02ef7f
Suraj Deshmukh <surajd.service@gmail.com>
2021-09-30 15:07:55 +0530

    Add OSX support to ssh-key-gen

--- local-bin/ssh-key-gen.sh
+++ local-bin/ssh-key-gen.sh
@@ -1,17 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+print_usage() {
+  echo "Please provide the suffix name of the key"
+  echo "e.g. ssh-key-gen.sh github personal"
+  echo "e.g. ssh-key-gen.sh github work"
+}
 
 name=$1
 if [ -z "${name}" ]; then
-  echo "Please provide the suffix name of the key"
-  echo "e.g. ssh-key-gen.sh github"
+  print_usage
   exit 1
 fi
 
+email_type=$2
+case $email_type in
+  personal)
+  email="surajd.service@gmail.com"
+  ;;
+  work)
+  email="suraj@kinvolk.io"
+  ;;
+  *)
+  print_usage
+  exit 1
+  ;;
+esac
+
 set -euo pipefail
 
-# Generate new key
-ssh-keygen -t rsa -b 8192 -f ~/.ssh/id_rsa."${name}" -N "" -C "surajd.service@gmail.com"
+# Detect OS
+os=$(uname)
+case $os in
+  Darwin)
+  key_type="ed25519"
+  ;;
+  Linux)
+  key_type="rsa"
+  ;;
+  *)
+  echo "unsupported OS: ${os}"
+  exit 1
+esac
 
-echo "Add the key in ~/.ssh/id_rsa.${name}.pub to https://github.com/settings/keys"
+# The file name of the SSH key.
+filename="${HOME}/.ssh/id_${key_type}.${name}.${email_type}"
+
+# Generate new key
+ssh-keygen -t "${key_type}" -b 8192 -f "${filename}" -N "" -C "${email}"
+
+echo "Add the key in ${filename}.pub to https://github.com/settings/keys"
 echo "After that run -> ssh -T git@github.com"
-cat ~/.ssh/id_rsa."${name}".pub
+cat "${filename}.pub"
