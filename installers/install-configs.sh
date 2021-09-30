7832aa57ee3b632fa122eb35416d3b7319d0f2e9
Suraj Deshmukh <surajd.service@gmail.com>
2021-09-30 16:13:10 +0530

    Add ZSH config
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- installers/install-configs.sh
+++ installers/install-configs.sh
@@ -4,7 +4,17 @@ set -euo pipefail
 
 echo "Installing configs from ./configs dir"
 
-ln -sf `pwd`/configs/bashrc ~/.bashrc
+# Install zshrc for OSX and bashrc for the rest.
+os=$(uname)
+case $os in
+  Darwin)
+  ln -sf `pwd`/configs/zshrc ~/.zshrc
+  ;;
+  *)
+  ln -sf `pwd`/configs/bashrc ~/.bashrc
+  ;;
+esac
+
 ln -sf `pwd`/configs/gitconfig ~/.gitconfig
 ln -sf `pwd`/configs/gitignore ~/.gitignore
 ln -sf `pwd`/configs/terraformrc ~/.terraformrc
