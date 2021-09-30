eab8d528c6888bf69c5be6ed4e19323505a9c764
Suraj Deshmukh <surajd.service@gmail.com>
2021-09-30 23:39:32 +0530

    generate-password: Add OSX support
    Check if pass is installed. If not installed then error out.
    
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- local-bin/generate-password.sh
+++ local-bin/generate-password.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # generates random password for you
 
+# Check if pass is installed.
+which pass > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Install 'pass': https://www.passwordstore.org."
+    exit 1
+fi
+
 password=$(yes | pass generate foo 50 | tail -1)
 echo "${password}"
