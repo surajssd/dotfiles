ba9c659f17b181874106b988b1d663467e9eb7e3
Suraj Deshmukh <surajd.service@gmail.com>
2021-10-01 15:54:01 +0530

    fastmirror: Make it fedora exclusive
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- local-bin/fastmirror.sh
+++ local-bin/fastmirror.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
+cat /etc/dnf/dnf.conf > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Not a Fedora machine. Cannot execute."
+    exit 1
+fi
 
+set -euo pipefail
 #===================================================================
 # add fastest mirror
 cat /etc/dnf/dnf.conf | grep 'fastestmirror'
