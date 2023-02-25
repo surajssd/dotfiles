4aa5402a3a753c25211135dba9fd60d81917d324
Suraj Deshmukh <surajd.service@gmail.com>
2023-02-24 16:09:43 -0800

    Add ssh config
    This loads the ssh key automatically for github.
    
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- installers/install-configs.sh
+++ installers/install-configs.sh
@@ -7,17 +7,18 @@ echo "Installing configs from ./configs dir"
 # Install zshrc for OSX and bashrc for the rest.
 os=$(uname)
 case $os in
-  Darwin)
-  ln -sf `pwd`/configs/zshrc ~/.zshrc
+Darwin)
+  ln -sf $(pwd)/configs/zshrc ~/.zshrc
   ;;
-  *)
-  ln -sf `pwd`/configs/bashrc ~/.bashrc
+*)
+  ln -sf $(pwd)/configs/bashrc ~/.bashrc
   ;;
 esac
 
-ln -sf `pwd`/configs/gitconfig ~/.gitconfig
-ln -sf `pwd`/configs/gitignore ~/.gitignore
-ln -sf `pwd`/configs/terraformrc ~/.terraformrc
-ln -sf `pwd`/configs/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/configs/gitconfig ~/.gitconfig
+ln -sf $(pwd)/configs/gitignore ~/.gitignore
+ln -sf $(pwd)/configs/terraformrc ~/.terraformrc
+ln -sf $(pwd)/configs/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/configs/ssh_config ~/.ssh/config
 
 echo "Installation successful."
