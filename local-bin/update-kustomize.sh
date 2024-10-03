ca8115ee14f55af64c6eb8e95c0b7b92e54d816a
Suraj Deshmukh <surajd.service@gmail.com>
2024-10-02 21:06:31 -0700

    install-kustomize: Update script
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- local-bin/update-kustomize.sh
+++ local-bin/update-kustomize.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | sh
+pushd $(mktemp -d)
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
 
 mv kustomize ~/.local/bin/
 echo "Downloaded successfully in ~/.local/bin/"
+popd
