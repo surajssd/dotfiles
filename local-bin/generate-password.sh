902e143d0de1fd4f9831aa1645b16ed3fb0683ca
Suraj Deshmukh <surajd.service@gmail.com>
2024-11-22 16:47:12 -0800

    Remove newline from the password generator

--- local-bin/generate-password.sh
+++ local-bin/generate-password.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # Get the length of the password from the first argument or default to 20
 LENGTH=${1:-20}
 
-echo $(tr </dev/urandom -dc 'A-Za-z0-9!@#$%&*_-' | head -c "${LENGTH}" || true)
+echo -n $(tr </dev/urandom -dc 'A-Za-z0-9!@#$%&*_-' | head -c "${LENGTH}" || true)
