dace101838b699fa527094d57070bfbd82edc16d
Suraj Deshmukh <surajd.service@gmail.com>
2025-01-10 12:12:04 +0530

    Add script to replicate image from anywhere to quay

---
+++ local-bin/replicate-image-to-quay.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# mirrors an image from one registry to quay
+
+IMAGE_NAME=$1
+: "${IMAGE_NAME:?Usage: ${BASH_SOURCE[0]} <image_name>}"
+
+set -euo pipefail
+
+QUAY_REPO="quay.io/surajd"
+
+# An image could be coming from some other registry like bar.io/foo/baz, so remove the bar.io
+DOMAIN_LESS_IMAGE_NAME="${IMAGE_NAME#*/}"
+QUAY_IMAGE_NAME="${QUAY_REPO}/${DOMAIN_LESS_IMAGE_NAME}"
+
+docker pull "${IMAGE_NAME}"
+docker tag "${IMAGE_NAME}" "${QUAY_IMAGE_NAME}"
+docker push "${QUAY_REPO}/${IMAGE_NAME}"
