47683e9ed1a013cff36c59afeffcc35db8b91dc0
Suraj Deshmukh <surajd.service@gmail.com>
2025-03-11 16:04:29 -0700

    Add debug k8s node script
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

---
+++ local-bin/debug-node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# If user doesn't provide the node name as an argument then exit
+if [ $# -ne 1 ]; then
+    echo "Usage:"
+    echo "debug-node.sh <node-name>"
+    exit 1
+fi
+
+kubectl debug "node/${1}" --image=ubuntu -it -- chroot /host /bin/bash
