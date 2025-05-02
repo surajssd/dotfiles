ff88b40e917c67dae63f9f0deca94e9c6a5ded6e
Suraj Deshmukh <surajd.service@gmail.com>
2025-05-02 14:22:18 -0700

    util.sh: Remove the set -euo pipefail from the script
    This can be added by the individual scripts but it is not needed in a utility
    script. Because that brings in unintended problems.
    
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- local-bin/util.sh
+++ local-bin/util.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 # referenced from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 function get_latest_release() {
   version=$(curl --silent "https://api.github.com/repos/$1/releases/latest" |
