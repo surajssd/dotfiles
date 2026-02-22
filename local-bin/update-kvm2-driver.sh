d10211324b8589e59fc4d315b07ee0340be2a276
Suraj Deshmukh <surajd.service@gmail.com>
2026-02-22 10:45:16 -0800

    Delete update-kvm2-driver.sh

--- local-bin/update-kvm2-driver.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-set -x
-
-cd /tmp
-curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2
-install docker-machine-driver-kvm2 "${HOME}"/.local/bin
